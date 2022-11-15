4bf4d50eae9fe18e85dc75c6550c71fe3857e146
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-15 09:42:50 -0300

    Add files via upload

---
+++ QUESTÃO 5 (LISTA1).cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+	 
+	 int main (){
+	 	int rolos;
+	 	printf ("Digite o núnero de rolos");
+	 	scanf ("%d", &rolos);
+	 	float fios = rolos * 50;
+	 	printf ("%d",rolos,"rolos equivalem a %d", fios,"metros de fios");
+	}
