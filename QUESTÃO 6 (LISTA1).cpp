c075045ba4edc00c9d8b95d430ea21ae77a6e799
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-15 09:53:08 -0300

    Add files via upload

---
+++ QUESTÃO 6 (LISTA1).cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+	#include <ctype.h>
+	#include <string.h>
+	#include <stdlib.h>
+	 
+	 int main (void){
+	 float n1,n2,resultado;
+	 
+	 printf ("[DIGITE SUA PRIMEIRA E SUA SEGUNDA NOTA]");
+	 scanf ("%f""%f",&n1,&n2);
+	 
+	 if (n1 <= 3.5){
+	 	
+}
+	 else ("[NOTA INVALIDA]");
+	 
+	 if (n2 <= 7.5){
+	 	
+	 }
+	 	else ("[NOTA INVALIDA]");
+	 	
+	 	resultado = n1 =n2;
+	 	
+	 	printf ("[O RESULTADO É %f]",resultado);
+	 	
+	}
+
